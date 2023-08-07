0f6f311d59d85984bf4d12ea51c09e09dc26fb92
rodrigo183 <digo.marqualves@gmail.com>
2023-08-07 15:09:31 -0300

    first commit

---
+++ src/main/java/com/br/restaurantedelivery/RestauranteDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.br.restaurantedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/restaurantedelivery/RestauranteDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.restaurantedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
