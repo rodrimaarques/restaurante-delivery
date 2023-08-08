cbdc52baa81c5df9d05b54be7ed2fe2ffcf46f5f
rodrigo183 <digo.marqualves@gmail.com>
2023-08-08 13:31:15 -0300

    cria classe clienteDetalhadoResponse

---
+++ src/main/java/com/br/restaurantedelivery/cliente/application/api/response/ClienteDetalhadoResponse.java
@@ -0,0 +1,27 @@
+package com.br.restaurantedelivery.cliente.application.api.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.br.restaurantedelivery.cliente.domain.Cliente;
+
+import lombok.Value;
+
+@Value
+public class ClienteDetalhadoResponse {
+	
+	 private UUID idCliente;
+	    private String nomeCliente;
+	    private String telefone;
+	    private LocalDateTime dataHoraDoCadastro;
+	    private LocalDateTime dataDaAlteracaoDoCadastro;
+
+	    public ClienteDetalhadoResponse(Cliente cliente) {
+	        this.idCliente = cliente.getIdCliente();
+	        this.nomeCliente = cliente.getNomeCliente();
+	        this.telefone = cliente.getTelefone();
+	        this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();
+	        this.dataDaAlteracaoDoCadastro = cliente.getDataDaAlteracaoDoCadastro();
+	    }
+
+}
