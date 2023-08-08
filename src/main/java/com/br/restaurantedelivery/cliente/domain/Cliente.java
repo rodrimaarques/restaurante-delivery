90298f5df805efd72954683f42594762bfcc3690
rodrigo183 <digo.marqualves@gmail.com>
2023-08-08 13:18:05 -0300

    implementa campos no domain

--- src/main/java/com/br/restaurantedelivery/cliente/domain/Cliente.java
+++ src/main/java/com/br/restaurantedelivery/cliente/domain/Cliente.java
@@ -3,11 +3,14 @@ package com.br.restaurantedelivery.cliente.domain;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.br.restaurantedelivery.cliente.application.api.request.ClienteRequest;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -32,5 +35,27 @@ public class Cliente {
 	private String telefone;
 	private LocalDateTime dataHoraDoCadastro;
 	private LocalDateTime dataDaAlteracaoDoCadastro;
+	
+	
+	public Cliente(@NotBlank(message = "Insira o nome do cliente") String nomeCliente,
+			@NotBlank(message = "Insira um numero de telefone") String telefone, LocalDateTime dataHoraDoCadastro,
+			LocalDateTime dataDaAlteracaoDoCadastro) {
+		super();
+		this.nomeCliente = nomeCliente;
+		this.telefone = telefone;
+		this.dataHoraDoCadastro = LocalDateTime.now();
+		this.dataDaAlteracaoDoCadastro = LocalDateTime.now();
+	}
+
+
+	public Cliente(@Valid ClienteRequest clienteRequest) {
+		
+		this.nomeCliente = clienteRequest.getNomeCliente();
+		this.telefone = clienteRequest.getNomeCliente();
+		this.dataHoraDoCadastro = LocalDateTime.now();
+		this.dataDaAlteracaoDoCadastro = LocalDateTime.now();
+	}
+	
+	
 
 }
